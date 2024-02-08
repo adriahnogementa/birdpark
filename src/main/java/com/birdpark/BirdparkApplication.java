0846c376a6843d5a818f2bb70a497faa0ecb82db
Adrian Romanik <adrianromanik@googlemail.com>
2024-02-08 15:30:58 +0100

    first commit

---
+++ src/main/java/com/birdpark/BirdparkApplication.java
@@ -0,0 +1,13 @@
+package com.birdpark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BirdparkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BirdparkApplication.class, args);
+	}
+
+}
