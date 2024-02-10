2b02f62c7200a228f5756cc0ef64592fcdfa83e1
Loisployce <louis.dittmann@gmail.com>
2024-02-10 12:52:37 +0100

    Allow Cross-Origin

--- src/main/java/com/birdpark/BirdparkApplication.java
+++ src/main/java/com/birdpark/BirdparkApplication.java
@@ -2,6 +2,9 @@ package com.birdpark;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class BirdparkApplication {
@@ -10,4 +13,17 @@ public class BirdparkApplication {
 		SpringApplication.run(BirdparkApplication.class, args);
 	}
 
+	/**
+	 * Das ist nur eine Methode für Cross-Origin, also dass auf einem Pc der eine Port (React) auf den andern Port (Backend) zugreifen kann
+	 * @return
+	 */
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+			}
+		};
+	}
 }
